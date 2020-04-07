ba1df99804acf9d7c7e53fd608b3f26db9c5709a
samerlagha <samer_la@mail.ru>
2020-04-07 14:40:49 +0300

    commit

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -23,7 +23,9 @@ const App = () => {
         <ItemStatusFilter />
       </div>
 
-      <TodoList todos={todoData} />
+      <TodoList todos={todoData}
+       onDeleted ={(id)=>console.log('delet',id)}
+      />
     </div>
   );
 };
--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -3,41 +3,69 @@ import React from 'react';
 import './todo-list-item.css';
 
 class TodoListItem extends React.Component{
-
-  
+  state ={
+    done:false,
+    important:false
+  }
+  // constructor(props){
+  //   super(props);
+  //    this.state= {
+  //     done:false
+  //    }
+  //   };
+  // }
+onLabelClick =()=>{
+    this.setState(({done})=>{
+    
+      return{
+        done: !done
+      }
+    })};
+onMarkImportant = ()=>{
+   this.setState(({important})=>{
+     return {
+       important: !important
+     };
+   });
+};
   render(){
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { label, onDeleted } = this.props;
+    const {done,important} =this.state;
+    let classNames='todo-list-item';
+    if(done){
+      classNames += ' done ';
+    }
+    if(important){
+      classNames += ' important ';
+    }
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}>
+          
+           onClick={this.onLabelClick}
+          >
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                
+                onClick={this.onMarkImportant}
+                >
           <i className="fa fa-exclamation" />
         </button>
   
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                
+                onClick ={onDeleted}
+                >
           <i className="fa fa-trash-o" />
         </button>
       </span>
     );
   }
+
 }
-
-const TodoListItem = ({ label, important = false }) => {
-
- 
-
-  
-};
-
 export default TodoListItem;
